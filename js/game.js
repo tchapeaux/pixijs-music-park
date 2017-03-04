bf886c231b28eb8f19761d75b9ae942b08076598
Thomas Chapeaux <chapeauxthomas@gmail.com>
2017-03-04 18:24:28 +0100

    Read level from JSON (basic implementation)

--- js/game.js
+++ js/game.js
@@ -1,24 +1,7 @@
 function Game() {
-    var farTexture = PIXI.Texture.fromImage("resources/bg-far-2.png");
-    this.far = new PIXI.extras.TilingSprite(farTexture, wScr, 256);
-    this.far.position.x = 0;
-    this.far.position.y = 0;
-    this.far.tilePosition.x = 0;
-    this.far.tilePosition.y = 0;
-    stage.addChild(this.far);
+    this.level = new Level("resources/map/map1.json");
 
-    var midTexture = PIXI.Texture.fromImage("resources/bg-mid-2.png");
-    this.mid = new PIXI.extras.TilingSprite(midTexture, wScr, 256);
-    this.mid.position.x = 0;
-    this.mid.position.y = hScr / 4;
-    this.mid.tilePosition.x = 0;
-    this.mid.tilePosition.y = 0;
-    stage.addChild(this.mid);
-
-    this.ground = new PIXI.Graphics();
-    this.ground.beginFill(0xFFFFFF);
-    this.ground.drawRect(0, hScr / 4 + 256, wScr, hScr);
-    stage.addChild(this.ground);
+    stage.addChild(this.level.container);
 
     this.player = new PIXI.Sprite.fromImage("resources/djembeman.png");
     this.player.position.x = 10;
@@ -41,9 +24,6 @@ Game.prototype.update = function(ds) {
     } else if (keysPressed.has(40) /* DOWN ARROW*/) {
         direction_y += 1;
     }
-    this.far.tilePosition.x -= 0.128 * direction_x * ds;
-    this.mid.tilePosition.x -= 0.64 * direction_x * ds;
-
     this.player.position.x += 100 * direction_x * ds;
     this.player.position.y += 100 * direction_y * ds;
 
